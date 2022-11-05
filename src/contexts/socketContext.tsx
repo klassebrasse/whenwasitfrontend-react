70503b3b07faa1e2d62fed3010f66273c821210f
Anton Klasson <anton@leadtub.se>
2022-11-05 15:46:25 +0100

    server url

--- src/contexts/socketContext.tsx
+++ src/contexts/socketContext.tsx
@@ -4,7 +4,7 @@ import {ISocket} from "../types/ISocket";
 import {createContext, useContext, useEffect, useState} from "react";
 
 
-const socket = io();
+const socket = io("https://when-guess-server.herokuapp.com", { transports : ['websocket'], upgrade: false, forceNew: false});
 const SocketContext = createContext(socket);
 
 export function useSocket() {
@@ -15,7 +15,7 @@ export function SocketProvider({ children }: any) {
     //Add ISocket, store user data
     const [currentSocket, setCurrentSocket] = useState<ISocket>(null);
     useEffect(() => {
-
+        setCurrentSocket(io("https://when-guess-server.herokuapp.com", { transports : ['websocket'], upgrade: false, forceNew: false}))
 
         return () => setCurrentSocket(null)
     }, [])
